cf14503ad62518df5b5367276a5a915221d89806
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2024-11-14 18:15:13 -0800

    created vacumn subsystem

---
+++ src/main/java/frc/robot/subsystems/vacuumSubsystem.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class vacuumSubsystem extends SubsystemBase {
+  /** Creates a new vacuumSubsystem. */
+  public vacuumSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
