f95ce3e037976fc63165d370775d027655f90b2b
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2024-12-07 14:55:14 -0800

    extra commit

--- src/main/java/frc/robot/subsystems/vacuumSubsystem.java
+++ src/main/java/frc/robot/subsystems/vacuumSubsystem.java
@@ -13,13 +13,13 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class vacuumSubsystem extends SubsystemBase {
+public class VacuumSubsystem extends SubsystemBase {
   /** Creates a new VacuumSubsystem. */
 
   public TalonSRX vacuumMotor;
 
  
-  public vacuumSubsystem(int can) {
+  public VacuumSubsystem(int can) {
 
     vacuumMotor = new TalonSRX(can);
   }
