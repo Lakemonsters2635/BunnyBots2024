6f8375f892f5288b1011e59c2b057d96fa8f7e53
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2024-12-28 14:20:45 -0800

    fixed too many subscribers error

--- src/main/java/frc/robot/subsystems/StreamDeckSubsystem.java
+++ src/main/java/frc/robot/subsystems/StreamDeckSubsystem.java
@@ -4,10 +4,15 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.BooleanEntry;
+import edu.wpi.first.networktables.BooleanTopic;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StringEntry;
+import edu.wpi.first.networktables.StringTopic;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer.IndexedColorIterator;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class StreamDeckSubsystem extends SubsystemBase {
@@ -17,20 +22,31 @@ public class StreamDeckSubsystem extends SubsystemBase {
   private NetworkTable table;
   int listenerHandle;
 
+  private BooleanEntry button0Entry;
+  private StringEntry selectedProgramEntry;
+
   public StreamDeckSubsystem() {
     ntinst = NetworkTableInstance.getDefault();
     ntinst.setServer("127.0.0.1");
     table = ntinst.getTable("StreamDeck");
     ntinst.removeListener(0);
 
+    button0Entry = table.getBooleanTopic("0").getEntry(false);
+    selectedProgramEntry = table.getStringTopic("SelectedProgramString2").getEntry("");
+
     clear();
   }
 
   public boolean isPressed(int index){
-    boolean val = table.getBooleanTopic(index+"").getEntry(false).getAsBoolean();
-    table.getBooleanTopic(index+"").getEntry(false).close();
-    clear();
+    boolean val = button0Entry.getAsBoolean();
+    // table.getBooleanTopic(index+"").getEntry(false).close();
+    // clear();
     return val;
+  }
+
+  public String getSelectedProgram() {
+    String selectedProgram = selectedProgramEntry.get();
+    return selectedProgram;
   }
 
   public void clear(){
@@ -43,12 +59,14 @@ public class StreamDeckSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    if(isPressed(0)){
-      System.out.println("INDEX 0 IS PRESSED");
-    }
-    else{
-      System.out.println("INDEX 0 IS NOT PRESSED");
-    }
+    // if(isPressed(0)){
+    //   System.out.println("INDEX 0 IS PRESSED");
+    // }
+    // else{
+    //   System.out.println("INDEX 0 IS NOT PRESSED");
+    // }
     // This method will be called once per scheduler run
+
+    SmartDashboard.putString("selectedProgramTopic", getSelectedProgram());
   }
 }
